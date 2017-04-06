8b8da6c7c74ac0742380ac892ea385b945fc6e04
meli <melatbeyene04@gmail.com>
2017-04-06 12:26:26 -0700

    Assignment 1 tips calculator

---
+++ assignment_1.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="assignment_1.aspx.cs" Inherits="assignment_1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <style>
+body {
+    background-color: palevioletred;
+}
+
+h1 {
+    color: white;
+    text-align: center;
+}
+
+p {
+    font-family: verdana;
+    font-size: 20px;
+} </style> 
+
+<head runat="server">
+
+    <title>calculating tips</title>
+
+</head>
+
+<body>
+    <center> 
+    <form id="form1" runat="server">
+        <h1> Tip Calculator</h1>
+   
+
+        <p> Meal Amount <asp:TextBox ID="MealTextBox" runat="server"></asp:TextBox> </p>
+        <p> choose your percent tip 
+            <br />
+
+            <asp:RadioButtonList ID="TipPercentsRadioButtonList" runat="server" Height="73px" Width="94px">
+           
+                </asp:RadioButtonList>
+           
+            <br />
+            <p> other </p>
+            <asp:TextBox ID="othertextbox" runat="server"></asp:TextBox>
+
+            <br />
+            <asp:Button ID="submitbutton" runat="server" Text="submit" OnClick="submitbutton_Click" Height="21px" Width="109px" />
+            &nbsp;&nbsp;
+        <br />
+&nbsp;<br />
+
+            <asp:Label ID="resultlable" runat="server" Text="Label"></asp:Label>
+
+
+  
+    </form>
+        </center>
+</body>
+</html>
---
+++ assignment_1.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class assignment_1 : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        if (!IsPostBack)
+        {
+
+            string[] percents = { "10%", "15%", "20%", "other" };
+            TipPercentsRadioButtonList.DataSource = percents;
+            TipPercentsRadioButtonList.DataBind();
+
+        }
+    }
+    protected void submitbutton_Click(object sender, EventArgs e)
+    {
+        GetTotals();
+    }
+
+    private void GetTotals()
+
+    {
+        double amount;
+        Tip tip = null;
+
+        bool goodamount = double.TryParse(MealTextBox.Text, out amount);
+        if (goodamount)
+        {
+            double percent = 0;
+            if (TipPercentsRadioButtonList.SelectedItem.Text != " other")
+            {
+
+                if (TipPercentsRadioButtonList.SelectedItem.Text.Equals("10%"))
+                    percent = .1;
+                if (TipPercentsRadioButtonList.SelectedItem.Text.Equals("15%"))
+                    percent = .15;
+                if (TipPercentsRadioButtonList.SelectedItem.Text.Equals("20%"))
+                    percent = .2;
+            }
+            else
+            {
+                percent = double.Parse(othertextbox.Text);
+                percent /= 100; //percent = percent
+            }
+
+
+
+            tip = new Tip(amount, percent);
+        }
+
+        else
+        {
+            Response.Write("<script>alert('enter a valid amount ');</script>");
+
+        }
+
+
+        resultlable.Text = "Amount:"
+
+            + amount.ToString("$ #,##0.00") + " <br/>" + "Tax:"
+
+            + tip.calculateTax().ToString("$ #,##0.00")
+
+            + "<br/>" + "tip:" + tip.calculateTip().ToString("$ #,##0.00") + "<br/>" + "Total:"
+
+              + tip.Total().ToString("$ #,##0.00");
+    }
+}
