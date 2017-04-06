38de032dad500e00c58113583cffe357e8417ba6
meli <melatbeyene04@gmail.com>
2017-04-06 12:27:44 -0700

    Add files via upload

---
+++ tip/Tip.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Tip
+/// </summary>
+public class Tip
+{
+    private double mealAmount;
+    private double tipPercent;
+    private const double TAXPERCENT = .101;
+    public Tip( double amount, double tipPerc)
+    {
+        mealAmount = amount;
+        tipPercent = tipPerc;
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public double  calculateTax()
+    {
+        return mealAmount * TAXPERCENT;
+
+    }
+
+    public double calculateTip()
+    {
+        return mealAmount * tipPercent; 
+    }
+        public double Total()
+    {
+        return mealAmount + calculateTax() + calculateTip();
+            
+            }
+        
+        }
